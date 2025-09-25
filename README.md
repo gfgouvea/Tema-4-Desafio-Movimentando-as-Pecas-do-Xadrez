fa2bcf134cc554b3354d6c59577cb6ef8f98466b
Guilherme Gouvea <gfgouvea@gmail.com>
2025-09-25 13:05:49 +0000

    Entrada do código completo desafio nivel novato.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Tema-4-Desafio-Movimentando-as-Pecas-do-Xadrez
+-implementação do primeiro código do desafio nível novato.
+-entrega do desavio nivel novato em 25/09/2025
---
+++ movimentacaoXadrez.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main(){
+    int i = 0;
+
+    printf("Movimentação da TORRE.\n\n");
+    for (int i = 0; i < 5; i++)
+    {
+        printf("Direita\n");
+    }
+    printf("\nmovimento da TORRE finalizada.\n");
+    printf("\n===================================\n\n");
+
+    printf("Movimentação do BISPO.\n\n");
+    do{
+        printf("Cima - Direita\n");
+        i++;
+    } while (i < 5); 
+    printf("\nmovimento do BISPO finalizada.\n");
+    printf("\n===================================\n\n");
+
+    printf("Movimentação da RAINHA.\n\n");
+    i = 0;
+    while (i < 8)
+    {
+        printf("Esquerda\n");
+        i++;
+    }
+    printf("\nmovimento da RAINHA finalizada.\n");
+    printf("\n===================================\n");
+    printf("\nFim do experiento. Obrigado!.\n\n\n");
+
+    return 0;
+}
